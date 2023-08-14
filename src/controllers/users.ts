94a15e4f8642be988cf9434d0156147f9b5451b6
Ericson Scodeler <ericson.br@hotmail.com>
2023-08-14 11:01:22 -0300

    update username

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express'
 
-import { deleteUserById, getUser } from '../db/users'
+import { deleteUserById, getUser, getUserById } from '../db/users'
 
 export const getAllUsers = async (req: Request, res: Response) => {
   try {
@@ -22,6 +22,28 @@ export const deleteUser = async (req: Request, res: Response) => {
     return res.json(deleteUser)
   } catch (error) {
     console.log(error)
+    return res.sendStatus(400)
+  }
+}
+
+export const updateUser = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params
+    const { username } = req.body
+
+    if (!username) {
+      return res.sendStatus(400)
+    }
+
+    const user = await getUserById(id)
+
+    user.username = username
+    await user.save()
+
+    return res.status(200).json(user).end()
+  } catch (error) {
+    console.log(error)
+
     return res.sendStatus(400)
   }
 }
